d385cfe6cf74fb1e47a9f22dbf8a6a77efcae229
Vitinho200 <100795023+Vitinho200@users.noreply.github.com>
2024-03-19 10:37:43 -0300

    Create User.js

---
+++ User.js
@@ -0,0 +1,31 @@
+function toggleMenu(menuItem) {
+    // Obtém o elemento do conteúdo
+    var content = document.getElementById("content");
+  
+    // Oculta ou exibe o conteúdo com base no item de menu selecionado
+    switch (menuItem) {
+      case 'perfil':
+        // Conteúdo do perfil
+        var profileContent = "<h2>Perfil do Usuário</h2>" +
+                             "<img src='perfil.jpg' alt='Imagem do Perfil'>" +
+                             "<p>Informações do perfil do usuário...</p>";
+        content.innerHTML = profileContent;
+        break;
+      case 'notificacoes':
+        content.innerHTML = "<h2>Notificações</h2><p>Conteúdo das Notificações...</p>";
+        break;
+      case 'privacidade':
+        content.innerHTML = "<h2>Privacidade</h2><p>Conteúdo da Privacidade...</p>";
+        break;
+      case 'conta':
+        content.innerHTML = "<h2>Conta</h2><p>Conteúdo da Conta...</p>";
+        break;
+      case 'sair':
+        // Redireciona o usuário para a página de login (ou qualquer outra página)
+        window.location.href = "login.html";
+        break;
+      default:
+        break;
+    }
+  }
+   

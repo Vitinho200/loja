94b5cd80f5cf489edda724ef092826130647ec3f
Vitinho200 <100795023+Vitinho200@users.noreply.github.com>
2024-04-10 07:45:46 -0300

    Atualizar o Login.js

--- Login.js
+++ Login.js
@@ -1,16 +1,53 @@
-document.getElementById("login-form").addEventListener("submit", function(event) {
-    event.preventDefault();
-    
-    // Obter os valores do formulário
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
-    
-    // Validar o login (só para fins de exemplo)
-    if (username === "vitor" && password === "senha11") {
-      alert("Login bem sucedido!");
-      // Redirecionar o usuário para outra página
-      window.location.href = "user.html"; // Coloque o nome do arquivo da página para onde deseja redirecionar
-    } else {
-      alert("Usuário ou senha incorretos. Tente novamente.");
-    }
-  });
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-analytics.js";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAHmZOaFQs0VAIfZGSsEchYia97bARYKt8",
+    authDomain: "verificacao-85263.firebaseapp.com",
+    projectId: "verificacao-85263",
+    storageBucket: "verificacao-85263.appspot.com",
+    messagingSenderId: "108475998338",
+    appId: "1:108475998338:android:debd6530e297e7dbfe549b"
+  };
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth();
+
+var email = document.getElementById("email");
+var password = document.getElementById("password");
+window.login= function(e) {
+  e.preventDefault();
+  var obj = {
+    email: email.value,
+    password: password.value,
+  };
+
+  signInWithEmailAndPassword(auth, obj.email, obj.password)
+    .then(function (success) {
+      alert("Login feito com Sucesso!")
+      var aaaa =  (success.user.uid);
+      localStorage.setItem("uid",aaaa)
+      console.log(aaaa)
+      
+      
+      
+      window.location.replace('user.html')
+     // localStorage.setItem(success,user,uid)
+      
+    })
+    .catch(function (err) {
+      alert("login error"+err);
+    });
+
+  console.log(obj);
+}
